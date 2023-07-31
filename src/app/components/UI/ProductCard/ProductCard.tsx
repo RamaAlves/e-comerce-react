fa231b18ac8f9b9bf4f09c2cb22b63e87b55fd82
RamaAlves <ramiroalvesprof@outlook.com>
2023-07-31 13:55:36 -0300

    add modal notification

--- src/app/components/UI/ProductCard/ProductCard.tsx
+++ src/app/components/UI/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { ProductSchema } from "../../../interfaces/interfaces";
 import { Card } from "../Card/Card";
 import { Loader } from "../Loader/Loader";
@@ -6,13 +6,18 @@ import { ContainerButtons } from "../ContainerButtons/ContainerButtons";
 import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
 import { useCart } from "../../../hooks/useCart";
+import { createPortal } from "react-dom";
+import { NotificationAddItem } from "../../Modal/Notification/NotificationAddItem/NotificationAddItem";
 
 type Product = {
   product: ProductSchema;
 };
 
 export function ProductCard({ product }: Product) {
+  const [showNotification, setShowNotification] = useState<boolean>(false);
   const { addItem } = useCart();
+
+
   return (
     <Card>
       <h3>{product.title}</h3>
@@ -32,8 +37,22 @@ export function ProductCard({ product }: Product) {
           <Button purple={false}>Details</Button>
         </Link>
 
-        <button onClick={()=>{addItem(product)}} /* purple={true} */>🛒</button>
+        <button onClick={() => {
+          addItem(product)
+          setShowNotification(true)
+        }} /* purple={true} */>🛒</button>
       </ContainerButtons>
+      {showNotification &&
+        createPortal(
+          <NotificationAddItem
+            product={product}
+            onRemove={():void => {
+              setShowNotification(false);
+            }}
+          />,
+          document.getElementById("portal") as HTMLElement,
+          "notification-add-item"
+        )}
     </Card>
   );
 }
