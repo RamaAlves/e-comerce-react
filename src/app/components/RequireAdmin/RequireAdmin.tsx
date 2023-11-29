37e170613d7f87206e73350ca2661b485c1289f6
Ramiro Alves <ramiroalvesprof@outlook.com>
2023-11-29 12:01:38 -0300

    refactor RequireAdmin

--- src/app/components/RequireAdmin/RequireAdmin.tsx
+++ src/app/components/RequireAdmin/RequireAdmin.tsx
@@ -1,20 +1,29 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 /* import { useAuth } from "../../hooks/useAuth"; */
 import { ChildrenType } from "../../interfaces/interfaces";
 import { useUser } from "../../hooks/useUser";
+import { useEffect } from "react";
 
 export function RequireAdmin({ children }: ChildrenType) {
   const { user } = useUser();
   const location = useLocation();
+  const navigator =useNavigate() 
 
-  if (user?.role != "admin") {
+  useEffect(() => {
+    if (user?.role != "admin") {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      navigator("/login", {state:{ from: location }, replace:true});
+    }
+  },[user]);
+  /* if (user?.role != "admin") {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
     return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-  else {
-    return <>{children}</>;  
-  }
+  } */
+  return <>{children}</>;
 }
