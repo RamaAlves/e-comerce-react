26015d11cacc52c0b23bc2c01fc743078aaf069c
Ramiro Alves <ramiroalvesprof@outlook.com>
2023-11-29 00:53:56 -0300

    fixed requireAdmin

--- src/app/components/RequireAdmin/RequireAdmin.tsx
+++ src/app/components/RequireAdmin/RequireAdmin.tsx
@@ -12,8 +12,8 @@ export function RequireAdmin({ children }: ChildrenType) {
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    <Navigate to="/login" state={{ from: location }} replace />;
+    return (<Navigate to="/login" state={{ from: location }} replace />);
   }
 
-  return children;
+  return (children);
 }
