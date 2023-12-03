9a524d01fddf141e2a52004da415619ace7572e4
Ramiro Alves <ramiroalvesprof@outlook.com>
2023-12-02 21:39:33 -0300

    add animations and icons theme

--- src/app/components/UI/ThemeButton/ThemeButton.tsx
+++ src/app/components/UI/ThemeButton/ThemeButton.tsx
@@ -9,7 +9,7 @@ export function ThemeButton() {
     //save themeMode in localStorage
     localStorage.setItem(DARK_MODE, JSON.stringify(state));
   }
-  return (
+/*   return (
     <div className={darkMode ? style.darkContainer : style.container}>
       <p>Lights:</p>
       <div className={style.interruptor}>
@@ -31,6 +31,36 @@ export function ThemeButton() {
             ON
           </button>
         </div>
+      </div>
+    </div>
+  ); */
+  return (
+    <div className={style.container}>
+      <div className={style.icon}>
+        <p className={darkMode ? style.appear : style.disappear}>🌙</p>
+        <p className={!darkMode ? style.appear : style.disappear}>☀️</p>
+      </div>
+      <div
+        className={[
+          darkMode ? style.darkTheme : style.lightTheme,
+          style.containerInterruptor,
+        ].join(" ")}
+      >
+        <label className={style.interruptor}>
+          {/* <input
+            type="checkbox"
+            className={style.switch}
+            onClick={() => {
+              changeTheme(!darkMode);
+            }}
+          /> */}
+          <span
+            className={style.button}
+            onClick={() => {
+              changeTheme(!darkMode);
+            }}
+          ></span>
+        </label>
       </div>
     </div>
   );
